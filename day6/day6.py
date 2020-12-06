f9d0eab0e1427dba44f72bae6e7238c1fa199f15
Asko Seeba <1336066+askoseeba@users.noreply.github.com>
2020-12-06 12:54:23 +0200

    removed unnecessary list construction before set construction

--- day6/day6.py
+++ day6/day6.py
@@ -8,7 +8,7 @@ import numpy as np
 #
 
 with open(fname) as f:
-    print('Part 1: %d' % np.sum([len(set(list(line.replace('\n', '')))) for line in f.read()[:-1].split('\n\n')]))
+    print('Part 1: %d' % np.sum([len(set(line.replace('\n', ''))) for line in f.read()[:-1].split('\n\n')]))
 
 
 #
@@ -16,4 +16,4 @@ with open(fname) as f:
 #
 
 with open(fname) as f:
-    print('Part 2: %d' % np.sum([len(set.intersection(*[set(list(line)) for line in group.split('\n')])) for group in f.read()[:-1].split('\n\n')]))
+    print('Part 2: %d' % np.sum([len(set.intersection(*[set(line) for line in group.split('\n')])) for group in f.read()[:-1].split('\n\n')]))
