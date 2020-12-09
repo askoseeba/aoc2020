1e04e7085bbc159eeeceb9218b9f42990487ba08
Asko Seeba <1336066+askoseeba@users.noreply.github.com>
2020-12-09 19:29:33 +0200

    Day 9 v2 -- part 1 as shorter one-liner, bugfixes, part 2 more compact.

--- day9/day9-v2.py
+++ day9/day9-v2.py
@@ -13,18 +13,22 @@ with open(fname) as f:
 # Part 1 as oneliner, 10 times slower, though.
 #
 
-print('Part 1: %d' % any([not any([w1 + w2 == data[i] for w1, w2 in itertools.combinations(data[i - preamble_len:i], 2)]) for i in range(preamble_len, len(data))]))
+import itertools
+
+num = list(itertools.compress(data[preamble_len:], [not any([w1 + w2 == data[i] for w1, w2 in itertools.combinations(data[i - preamble_len:i], 2)]) for i in range(preamble_len, len(data))]))[0]
+
+print('Part 1: %d' % num)
 
 
 #
-# Part 2, more compact, almost as fast (about 25% slower)
+# Part 2, more compact, no speed impact
 #
 
 cs_len = 2
 while True:
-    if any([sum(data[cs_start : cs_start + cs_len]) == data[i] for cs_start in range(len(data) - cs_len + 1)]):
+    cs = [data[cs_start : cs_start + cs_len] for cs_start in range(len(data) - cs_len + 1) if sum(data[cs_start : cs_start + cs_len]) == num]
+    if len(cs) > 0:
         break
     cs_len += 1
-cs = data[cs_start : cs_start + cs_len]
-
-print('Part 2: %d' % (min(cs) + max(cs)))
+    
+print('Part 2: %d' % (min(cs[0]) + max(cs[0])))
